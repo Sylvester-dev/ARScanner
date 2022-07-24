59613a9810c81d87ab9bb5365574624b0693ca1f
Vineet Kumar <vk.vempire@gmail.com>
2022-07-25 03:19:07 +0530

    3d world

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'scanner.dart';
+import '3dviewer.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,9 +17,54 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scanner(),
+      home: Home(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.indigo[900],
+          bottom: const TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(
+                  Icons.qr_code_scanner,
+                  size: 40,
+                ),
+              ),
+              Tab(
+                icon: Icon(
+                  Icons.window_rounded,
+                  size: 40,
+                ),
+              ),
+            ],
+          ),
+          title: Text(
+            'AR MetaWorld',
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Scanner(),
+            Viewer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/scanner.dart
+++ lib/scanner.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_qr_bar_scanner/qr_bar_scanner_camera.dart';
-import 'package:arkit_plugin/arkit_plugin.dart';
-import 'dart:async';
-import 'package:vector_math/vector_math_64.dart' hide Colors;
+import 'package:babylonjs_viewer/babylonjs_viewer.dart';
 
 class Scanner extends StatefulWidget {
   Scanner({Key? key}) : super(key: key);
@@ -12,10 +10,8 @@ class Scanner extends StatefulWidget {
 }
 
 class _ScannerState extends State<Scanner> {
-  String? qrInfo = 'Scan a QR/Bar code';
+  String? qrInfo = 'No Result!!';
   bool camState = false;
-  late ARKitController arkitController;
-  Timer? timer;
 
   qrCallback(String? code) {
     setState(() {
@@ -29,38 +25,6 @@ class _ScannerState extends State<Scanner> {
     super.initState();
     setState(() {
       camState = true;
-    });
-  }
-
-  @override
-  void dispose() {
-    timer?.cancel();
-    arkitController.dispose();
-    super.dispose();
-  }
-
-  void onARKitViewCreated(ARKitController arkitController) {
-    this.arkitController = arkitController;
-
-    final material = ARKitMaterial(
-      lightingModelName: ARKitLightingModel.lambert,
-      diffuse: ARKitMaterialProperty.image("https://static.turbosquid.com/Preview/2021/01/07__02_00_21/SI0000.png2622FAD3-C0F0-4DB2-B67A-00CD10D6ECC0Zoom.jpg"),
-    );
-    final sphere = ARKitSphere(
-      materials: [material],
-      radius: 0.1,
-    );
-
-    final node = ARKitNode(
-      geometry: sphere,
-      position: Vector3(0, 0, -0.5),
-    );
-    this.arkitController.add(node);
-
-    timer = Timer.periodic(const Duration(milliseconds: 50), (timer) {
-      final rotation = node.eulerAngles;
-      rotation.x += 0.01;
-      node.eulerAngles = rotation;
     });
   }
 
@@ -99,8 +63,8 @@ class _ScannerState extends State<Scanner> {
               ),
             )
           : Center(
-              child: ARKitSceneView(
-                onARKitViewCreated: onARKitViewCreated,
+              child: BabylonJSViewer(
+                src: qrInfo.toString(),
               ),
             ),
     );
